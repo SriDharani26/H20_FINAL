40b005f0b5635495fe32f190461912e10352618e
Sri Dharani R <sridharanirajendran26@gmail.com>
2024-10-27 11:55:27 +0530

    commitfirst

--- traditional/app/game.tsx
+++ traditional/app/game.tsx
@@ -4,7 +4,7 @@ export default function game() {
   return (
     <View
       style={styles.container}>
-      <Text style={styles.text}>welcome</Text>
+      <Text style={styles.text}></Text>
     </View>
   );
 }
