ffaf9e6e5e83de984233b6376f54a33145c9a545
Sri Dharani R <sridharanirajendran26@gmail.com>
2024-10-27 11:52:46 +0530

    firstcommit

---
+++ traditional/app/game.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function game() {
+  return (
+    <View
+      style={styles.container}>
+      <Text style={styles.text}>welcome</Text>
+    </View>
+  );
+}
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#25292e',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  text:{
+    color:'#fff',
+  }
+})
